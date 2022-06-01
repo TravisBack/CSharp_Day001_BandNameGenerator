75cc7e420c1c7d152616a5ea808a9118635874e3
travisback <travisback@icloud.com>
2022-06-01 15:28:10 -0700

    Added proper Readline statements to get input

--- CSharp_Day001_BandNameGenerator/Program.cs
+++ CSharp_Day001_BandNameGenerator/Program.cs
@@ -4,9 +4,20 @@ namespace CSharp_Day001_BandNameGenerator
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Band Name Generator (v1.0)");
+
+            Console.Write("What city did you grow up in? ");
+            string city = Console.ReadLine();
+
+            Console.Write("What is the name of your pet? ");
+            string pet = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Your band name is: " + city + " " + pet);
         }
     }
 }
